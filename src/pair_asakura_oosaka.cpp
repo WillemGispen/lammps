77e89ab00b42894b5476ea45ea99cbdc9d6a910c
Willem Gispen <willemgispen@outlook.com>
2021-05-11 17:45:39 +0200

    Copy pair_hardyukawa for Asakura-Oosaka potential

---
+++ src/pair_asakura_oosaka.cpp
@@ -0,0 +1,383 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   https://lammps.sandia.gov/, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+/*
+Hard-core repulsive Yukawa potential
+
+u(r) = A / r * exp(- k (r - 1))    , r > 1
+u(r) = LJ(r)                       , r < 1
+
+Assumed: particles have diameter 1
+*/
+
+#include "pair_asakura_oosaka.h"
+
+#include <cmath>
+#include "atom.h"
+#include "force.h"
+#include "comm.h"
+#include "neigh_list.h"
+#include "memory.h"
+#include "error.h"
+
+
+using namespace LAMMPS_NS;
+
+/* ---------------------------------------------------------------------- */
+
+PairAsakuraOosaka::PairAsakuraOosaka(LAMMPS *lmp) : Pair(lmp)
+{
+  writedata = 1;
+}
+
+/* ---------------------------------------------------------------------- */
+
+PairAsakuraOosaka::~PairAsakuraOosaka()
+{
+  if (allocated) {
+    memory->destroy(setflag);
+    memory->destroy(cutsq);
+
+    memory->destroy(rad);
+    memory->destroy(cut);
+    memory->destroy(a);
+    memory->destroy(offset);
+  }
+}
+
+/* ---------------------------------------------------------------------- */
+
+void PairAsakuraOosaka::compute(int eflag, int vflag)
+{
+  int i,j,ii,jj,inum,jnum,itype,jtype;
+  double xtmp,ytmp,ztmp,delx,dely,delz,evdwl,fpair;
+  double rsq,r2inv,r,rinv,screening,forceyukawa,factor;
+  double r6inv, r12inv, r24inv, r48inv, b5049;
+  int *ilist,*jlist,*numneigh,**firstneigh;
+
+  evdwl = 0.0;
+  ev_init(eflag,vflag);
+
+  double **x = atom->x;
+  double **f = atom->f;
+  int *type = atom->type;
+  int nlocal = atom->nlocal;
+  double *special_lj = force->special_lj;
+  int newton_pair = force->newton_pair;
+
+  inum = list->inum;
+  ilist = list->ilist;
+  numneigh = list->numneigh;
+  firstneigh = list->firstneigh;
+
+  // loop over neighbors of my atoms
+
+  for (ii = 0; ii < inum; ii++) {
+    i = ilist[ii];
+    xtmp = x[i][0];
+    ytmp = x[i][1];
+    ztmp = x[i][2];
+    itype = type[i];
+    jlist = firstneigh[i];
+    jnum = numneigh[i];
+
+    for (jj = 0; jj < jnum; jj++) {
+      j = jlist[jj];
+      factor = special_lj[sbmask(j)];
+      j &= NEIGHMASK;
+
+      delx = xtmp - x[j][0];
+      dely = ytmp - x[j][1];
+      delz = ztmp - x[j][2];
+      rsq = delx*delx + dely*dely + delz*delz;
+      jtype = type[j];
+
+      if (rsq < cutsq[itype][jtype]) {
+        r2inv = 1.0/rsq;
+        r = sqrt(rsq);
+        rinv = 1.0/r;
+        screening = exp(-kappa*(r-1.0));
+        forceyukawa = screening * a[itype][jtype] * (kappa + rinv) * r2inv;
+
+        if (r < 50.0/49.0) {
+          // add continuous hard sphere approx WCA(50,49)
+          r6inv = r2inv*r2inv*r2inv;
+          r12inv = r6inv * r6inv;
+          r24inv = r12inv * r12inv;
+          r48inv = r24inv * r24inv;
+          b5049 = 134.55266;
+          forceyukawa += T * 2.0 / 3.0 * b5049 * r48inv * r2inv * r2inv * (50.0 - 49.0 * r);
+        }
+
+        fpair = factor * forceyukawa;
+
+        f[i][0] += delx*fpair;
+        f[i][1] += dely*fpair;
+        f[i][2] += delz*fpair;
+        if (newton_pair || j < nlocal) {
+          f[j][0] -= delx*fpair;
+          f[j][1] -= dely*fpair;
+          f[j][2] -= delz*fpair;
+        }
+
+        if (eflag) {
+          evdwl = a[itype][jtype] * screening * rinv - offset[itype][jtype];
+          if (r < 50.0/49.0) {
+            // add continuous hard sphere approx WCA(50,49)
+            evdwl += T * 2.0 / 3.0 * b5049 * r48inv * (r2inv - rinv);
+            evdwl += T * 2.0 / 3.0;
+          }
+          evdwl *= factor;
+        }
+
+        if (evflag) ev_tally(i,j,nlocal,newton_pair,
+                             evdwl,0.0,fpair,delx,dely,delz);
+      }
+    }
+  }
+
+  if (vflag_fdotr) virial_fdotr_compute();
+}
+
+/* ----------------------------------------------------------------------
+   allocate all arrays
+------------------------------------------------------------------------- */
+
+void PairAsakuraOosaka::allocate()
+{
+  allocated = 1;
+  int n = atom->ntypes;
+
+  memory->create(setflag,n+1,n+1,"pair:setflag");
+  for (int i = 1; i <= n; i++)
+    for (int j = i; j <= n; j++)
+      setflag[i][j] = 0;
+
+  memory->create(cutsq,n+1,n+1,"pair:cutsq");
+  memory->create(rad,n+1,"pair:rad");
+  memory->create(cut,n+1,n+1,"pair:cut");
+  memory->create(a,n+1,n+1,"pair:a");
+  memory->create(offset,n+1,n+1,"pair:offset");
+}
+
+/* ----------------------------------------------------------------------
+   global settings
+------------------------------------------------------------------------- */
+
+void PairAsakuraOosaka::settings(int narg, char **arg)
+{
+  if (narg != 3) error->all(FLERR,"Illegal pair_style command");
+
+  kappa = utils::numeric(FLERR,arg[0],false,lmp);
+  cut_global = utils::numeric(FLERR,arg[1],false,lmp);
+  T = utils::numeric(FLERR,arg[2],false,lmp);  // temperature
+
+  // reset cutoffs that have been explicitly set
+
+  if (allocated) {
+    int i,j;
+    for (i = 1; i <= atom->ntypes; i++)
+      for (j = i; j <= atom->ntypes; j++)
+        if (setflag[i][j]) cut[i][j] = cut_global;
+  }
+}
+
+/* ----------------------------------------------------------------------
+   set coeffs for one or more type pairs
+------------------------------------------------------------------------- */
+
+void PairAsakuraOosaka::coeff(int narg, char **arg)
+{
+  if (narg < 3 || narg > 4)
+    error->all(FLERR,"Incorrect args for pair coefficients");
+  if (!allocated) allocate();
+
+  int ilo,ihi,jlo,jhi;
+  utils::bounds(FLERR,arg[0],1,atom->ntypes,ilo,ihi,error);
+  utils::bounds(FLERR,arg[1],1,atom->ntypes,jlo,jhi,error);
+
+  double a_one = utils::numeric(FLERR,arg[2],false,lmp);
+
+  double cut_one = cut_global;
+  if (narg == 4) cut_one = utils::numeric(FLERR,arg[3],false,lmp);
+
+  int count = 0;
+  for (int i = ilo; i <= ihi; i++) {
+    for (int j = MAX(jlo,i); j <= jhi; j++) {
+      a[i][j] = a_one;
+      cut[i][j] = cut_one;
+      setflag[i][j] = 1;
+      count++;
+    }
+  }
+
+  if (count == 0) error->all(FLERR,"Incorrect args for pair coefficients");
+}
+
+/* ----------------------------------------------------------------------
+   init for one type pair i,j and corresponding j,i
+------------------------------------------------------------------------- */
+
+double PairAsakuraOosaka::init_one(int i, int j)
+{
+  if (setflag[i][j] == 0) {
+    a[i][j] = mix_energy(a[i][i],a[j][j],1.0,1.0);
+    cut[i][j] = mix_distance(cut[i][i],cut[j][j]);
+  }
+
+  if (offset_flag && (cut[i][j] > 0.0)) {
+    double screening = exp(-kappa * (cut[i][j] - 1.0));
+    offset[i][j] = a[i][j] * screening / cut[i][j];
+  } else offset[i][j] = 0.0;
+
+  a[j][i] = a[i][j];
+  offset[j][i] = offset[i][j];
+
+  return cut[i][j];
+}
+
+/* ----------------------------------------------------------------------
+   proc 0 writes to restart file
+------------------------------------------------------------------------- */
+
+void PairAsakuraOosaka::write_restart(FILE *fp)
+{
+  write_restart_settings(fp);
+
+  int i,j;
+  for (i = 1; i <= atom->ntypes; i++)
+    for (j = i; j <= atom->ntypes; j++) {
+      fwrite(&setflag[i][j],sizeof(int),1,fp);
+      if (setflag[i][j]) {
+        fwrite(&a[i][j],sizeof(double),1,fp);
+        fwrite(&cut[i][j],sizeof(double),1,fp);
+      }
+    }
+}
+
+/* ----------------------------------------------------------------------
+   proc 0 reads from restart file, bcasts
+------------------------------------------------------------------------- */
+
+void PairAsakuraOosaka::read_restart(FILE *fp)
+{
+  read_restart_settings(fp);
+
+  allocate();
+
+  int i,j;
+  int me = comm->me;
+  for (i = 1; i <= atom->ntypes; i++)
+    for (j = i; j <= atom->ntypes; j++) {
+      if (me == 0) utils::sfread(FLERR,&setflag[i][j],sizeof(int),1,fp,nullptr,error);
+      MPI_Bcast(&setflag[i][j],1,MPI_INT,0,world);
+      if (setflag[i][j]) {
+        if (me == 0) {
+          utils::sfread(FLERR,&a[i][j],sizeof(double),1,fp,nullptr,error);
+          utils::sfread(FLERR,&cut[i][j],sizeof(double),1,fp,nullptr,error);
+        }
+        MPI_Bcast(&a[i][j],1,MPI_DOUBLE,0,world);
+        MPI_Bcast(&cut[i][j],1,MPI_DOUBLE,0,world);
+      }
+    }
+}
+
+/* ----------------------------------------------------------------------
+   proc 0 writes to restart file
+------------------------------------------------------------------------- */
+
+void PairAsakuraOosaka::write_restart_settings(FILE *fp)
+{
+  fwrite(&kappa,sizeof(double),1,fp);
+  fwrite(&cut_global,sizeof(double),1,fp);
+  fwrite(&offset_flag,sizeof(int),1,fp);
+  fwrite(&mix_flag,sizeof(int),1,fp);
+}
+
+/* ----------------------------------------------------------------------
+   proc 0 reads from restart file, bcasts
+------------------------------------------------------------------------- */
+
+void PairAsakuraOosaka::read_restart_settings(FILE *fp)
+{
+  if (comm->me == 0) {
+    utils::sfread(FLERR,&kappa,sizeof(double),1,fp,nullptr,error);
+    utils::sfread(FLERR,&cut_global,sizeof(double),1,fp,nullptr,error);
+    utils::sfread(FLERR,&offset_flag,sizeof(int),1,fp,nullptr,error);
+    utils::sfread(FLERR,&mix_flag,sizeof(int),1,fp,nullptr,error);
+  }
+  MPI_Bcast(&kappa,1,MPI_DOUBLE,0,world);
+  MPI_Bcast(&cut_global,1,MPI_DOUBLE,0,world);
+  MPI_Bcast(&offset_flag,1,MPI_INT,0,world);
+  MPI_Bcast(&mix_flag,1,MPI_INT,0,world);
+}
+
+/* ----------------------------------------------------------------------
+   proc 0 writes to data file
+------------------------------------------------------------------------- */
+
+void PairAsakuraOosaka::write_data(FILE *fp)
+{
+  for (int i = 1; i <= atom->ntypes; i++)
+    fprintf(fp,"%d %g\n",i,a[i][i]);
+}
+
+/* ----------------------------------------------------------------------
+   proc 0 writes all pairs to data file
+------------------------------------------------------------------------- */
+
+void PairAsakuraOosaka::write_data_all(FILE *fp)
+{
+  for (int i = 1; i <= atom->ntypes; i++)
+    for (int j = i; j <= atom->ntypes; j++)
+      fprintf(fp,"%d %d %g %g\n",i,j,a[i][j],cut[i][j]);
+}
+
+/* ---------------------------------------------------------------------- */
+
+double PairAsakuraOosaka::single(int /*i*/, int /*j*/, int itype, int jtype, double rsq,
+                          double /*factor_coul*/, double factor_lj,
+                          double &fforce)
+{
+  double r2inv,r,rinv,screening,forceyukawa,phi;
+  double r6inv, r12inv, r24inv, r48inv, b5049;
+
+  r2inv = 1.0/rsq;
+  r = sqrt(rsq);
+  rinv = 1.0/r;
+  screening = exp(-kappa*(r-1.0));
+  forceyukawa = a[itype][jtype] * r2inv * screening * (kappa + rinv);
+
+  if (r < 50.0/49.0) {
+    // add continuous hard sphere approx WCA(50,49)
+    r6inv = r2inv*r2inv*r2inv;
+    r12inv = r6inv * r6inv;
+    r24inv = r12inv * r12inv;
+    r48inv = r24inv * r24inv;
+    b5049 = 134.55266;
+    forceyukawa += T * 2.0 / 3.0 * b5049 * r48inv * r2inv * r2inv * (50.0 - 49.0 * r);
+  }
+
+  fforce = factor_lj * forceyukawa;
+
+  phi = a[itype][jtype] * screening * rinv - offset[itype][jtype];
+
+  if (r < 50.0/49.0) {
+    // add continuous hard sphere approx WCA(50,49)
+    phi += T * 2.0 / 3.0 * b5049 * r48inv * (r2inv - rinv);
+    phi += T * 2.0 / 3.0;
+  }
+
+  return factor_lj*phi;
+
+}
